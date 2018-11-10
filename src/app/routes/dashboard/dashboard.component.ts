c03b1f8349962df31d90e11f36835f8485a55a62
taogeT <zwtzjd@gmail.com>
2018-11-10 22:14:04 +0800

    develop dashboard title and footer

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
+import { SettingsService } from '@delon/theme';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
+  username: String;
 
   constructor(
-    private http: _HttpClient
+    private http: _HttpClient,
+    private settingService: SettingsService
   ) { }
 
   ngOnInit() {
+    this.username = this.settingService.user.name;
   }
 
 }
