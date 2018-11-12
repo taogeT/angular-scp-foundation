82c5ad137bd649bb85621a141b54c23b47a39d67
taogeT <zwtzjd@gmail.com>
2018-11-12 18:57:25 +0800

    add new component: dashboard shortcut.

---
+++ src/app/routes/dashboard/components/shortcut.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { of } from 'rxjs';
+import { filter, take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-dashboard-shortcut',
+  template: `
+    <nz-card [nzBordered]="false" [nzExtra]="extraTemplate" [nzTitle]="title" [nzLoading]="loading">
+      <ng-template #extraTemplate>
+        <a>更多</a>
+      </ng-template>
+
+    </nz-card>
+  `,
+  styles: []
+})
+export class ShortcutComponent implements OnInit {
+  tips: { key: string, title: string }[] = [
+    { key: 'newTranslation', title: '新增翻译条目'},
+    { key: 'newOriginal', title: '新增原创条目'},
+    { key: 'choicestOriginal', title: '精品原创SCP'},
+    { key: 'choicestStory', title: '精品原创故事'}
+  ];
+  @Input() key: String;
+  title: String = '';
+  loading: Boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+    of(...this.tips).pipe(
+      filter(tip => tip.key === this.key),
+      take(1),
+    ).subscribe(tip => {
+      this.title = tip.title;
+    });
+    this.loading = false;
+  }
+
+}
--- src/app/routes/dashboard/dashboard.module.ts
+++ src/app/routes/dashboard/dashboard.module.ts
@@ -4,13 +4,15 @@ import { SharedModule } from '@shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
 import { NewsComponent } from './components/news.component';
 import { WebsiteComponent } from './components/website.component';
+import { ShortcutComponent } from './components/shortcut.component';
 
 @NgModule({
   imports: [ SharedModule ],
   declarations: [
     DashboardComponent,
     NewsComponent,
-    WebsiteComponent
+    WebsiteComponent,
+    ShortcutComponent
   ]
 })
 export class DashboardModule { }
