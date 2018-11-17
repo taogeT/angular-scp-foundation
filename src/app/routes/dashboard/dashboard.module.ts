f501c5752e5b79229f53518f7c0c91fc952e1de6
taogeT <zwtzjd@gmail.com>
2018-11-17 17:36:27 +0800

    position dashboard component.

--- src/app/routes/dashboard/dashboard.module.ts
+++ src/app/routes/dashboard/dashboard.module.ts
@@ -1,10 +1,10 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
-import { NewsComponent } from './components/news.component';
-import { WebsiteComponent } from './components/website.component';
-import { ShortcutComponent } from './components/shortcut.component';
+import { NewsComponent } from './news/news.component';
+import { WebsiteComponent } from './website/website.component';
+import { ShortcutComponent } from './shortcut/shortcut.component';
 
 @NgModule({
   imports: [ SharedModule ],
