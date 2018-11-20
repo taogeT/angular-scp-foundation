a2223a0437aa60fe134dc69efa7579679c81b01f
taogeT <zwtzjd@gmail.com>
2018-11-20 16:28:59 +0800

    feat(module:series): init series content component.

---
+++ src/app/routes/series/content/content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-series-content',
+  templateUrl: './content.component.html',
+  styles: []
+})
+export class SeriesContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/routes/series/series.module.ts
+++ src/app/routes/series/series.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@shared/shared.module';
 import { SeriesComponent } from './series.component';
+import { SeriesContentComponent } from './content/content.component';
 
 @NgModule({
   imports: [ SharedModule ],
-  declarations: [ SeriesComponent ]
+  declarations: [ SeriesComponent, SeriesContentComponent ]
 })
 export class SeriesModule { }
