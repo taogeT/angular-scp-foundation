c86edcf407af87a1b884642fe022581e6892083b
taogeT <zwtzjd@gmail.com>
2018-11-23 16:09:07 +0800

    chore(module:series): change breadcrumb title and color.

--- src/app/routes/series/series.component.ts
+++ src/app/routes/series/series.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { _HttpClient } from '@delon/theme';
 
